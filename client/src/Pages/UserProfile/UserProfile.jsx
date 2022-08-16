3ce4c3d2a90b47cc28355e9a4d0165b98a3700c4
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-16 23:35:51 +0430

    user profile page completed

--- client/src/Pages/UserProfile/UserProfile.jsx
+++ client/src/Pages/UserProfile/UserProfile.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { UserProfileLeft } from "../../Components/UserProfileLeft/UserProfileLeft";
 import "./UserProfile.css";
+import ProfileCard from "../../Components/ProfileCard/ProfileCard"
+import Projects from "../../Components/Projects/Projects"
+import RightSide from "../../Components/RightSide/RightSide"
+
 export const UserProfile = () => {
   return <div className="UserProfile">
         <UserProfileLeft />
+        <div className="profileCenter">
+            <ProfileCard />
+            <Projects />
+        </div>
+            <RightSide />
       </div>;
 };
