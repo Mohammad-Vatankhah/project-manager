cb88c40f2d5f3cef78e76e4c3eb8bacaba759e4b
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-16 23:18:56 +0430

    user profile page created and styled

---
+++ client/src/Pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { UserProfileLeft } from "../../Components/UserProfileLeft/UserProfileLeft";
+import "./UserProfile.css";
+export const UserProfile = () => {
+  return <div className="UserProfile">
+        <UserProfileLeft />
+      </div>;
+};
