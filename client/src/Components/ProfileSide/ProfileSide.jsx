47c0dabd111620453f62774fc63116f8ea9b201a
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-16 13:22:46 +0430

    a bug fixed

--- client/src/Components/ProfileSide/ProfileSide.jsx
+++ client/src/Components/ProfileSide/ProfileSide.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard/ProfileCard'
+import './ProfileSide.css'
 
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
         <LogoSearch />
+        <ProfileCard />
     </div>
   )
 }
