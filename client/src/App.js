4439baf2f85716f79b7068a2ff5f32d08182489a
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-16 11:01:05 +0430

    home page created and basic styles added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import "./App.css"
+import "./App.css";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <Home/>
     </div>
   );
 }
---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div className='profileSide'>profile</div>
+        <div className='postSide'>post</div>
+        <div className='rightSide'>right</div>
+    </div>
+  )
+}
+
+export default Home
