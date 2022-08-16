2897c822b1f35a4693a3852f5f8bd1de75ff82d6
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-16 23:35:23 +0430

    modified for profile page

--- client/src/Components/ProfileCard/ProfileCard.jsx
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -4,6 +4,8 @@ import Profile from "../../img/profileImg.jpg";
 import "./ProfileCard.css";
 
 const ProfileCard = () => {
+  const ProfilePage = true;
+
   return (
     <div className="ProfileCard">
       <div className="ProfileImages">
@@ -28,10 +30,19 @@ const ProfileCard = () => {
             <span>12345</span>
             <span>Followers</span>
           </div>
+          {ProfilePage && (
+            <>
+              <div className="vl"></div>
+              <div className="follow">
+                <span>3</span>
+                <span>Posts</span>
+              </div>
+            </>
+          )}
         </div>
         <hr />
       </div>
-      <span>My Profile</span>
+      {ProfilePage ? "" : <span>My Profile</span>}
     </div>
   );
 };
