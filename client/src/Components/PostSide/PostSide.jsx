e80b7c5d05d4d8d91f768216468228899197f278
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-16 22:16:31 +0430

    a bug fixed

--- client/src/Components/PostSide/PostSide.jsx
+++ client/src/Components/PostSide/PostSide.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Projects from '../Projects/Projects'
 import "./PostSide.css"
 const PostSide = () => {
   return (
     <div className='PostSide'>
         <button className='button' id='create-button'>Create new project</button>
+        <Projects />
     </div>
   )
 }
