49d9251ec88827c8d49ee6052b40273d56c36c2c
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-17 11:51:08 +0430

    modified

--- client/src/Pages/CreateProject/CreateProject.jsx
+++ client/src/Pages/CreateProject/CreateProject.jsx
@@ -5,7 +5,6 @@ import "./CreateProject.css";
 import { BsFillImageFill } from "react-icons/bs";
 import { UilTimes } from "@iconscout/react-unicons";
 
-
 const CreateProject = () => {
   const [image, setImage] = useState(null);
   const imageRef = useRef();
@@ -30,55 +29,64 @@ const CreateProject = () => {
     }
   };
   return (
-    <div className="CreateProject">
-      <h1>Create New Project</h1>
-      <div className="inputs">
-        <div className="line1">
-          <input type="text" placeholder="Project Name" />
-          <AsyncSelect
-            value={selectedCompany}
-            styles={style}
-            placeholder={"Company"}
-            className="select"
-          ></AsyncSelect>
-        </div>
-        <div className="line2">
-          <AsyncSelect
-            className="select-cw"
-            isMulti
-            styles={style}
-            value={selectedCoworkers}
-            placeholder={"Co-Workers"}
-          ></AsyncSelect>
-        </div>
-        <div className="line3">
-          <textarea
-            className="description"
-            cols="30"
-            rows="10"
-            placeholder="Description"
-          ></textarea>
-        </div>
-        <div className="line4" style={{ display: "none" }}>
-          <input
-            type="file"
-            name="myImage"
-            ref={imageRef}
-            onChange={onImageChange}
-          />
-        </div>
-        <div className="addImage" onClick={() => imageRef.current.click()}>
-          <BsFillImageFill
-            style={{ fontSize: 30, position: "relative", zIndex: 1000 }}
-          />
-          <p>Add Photo</p>
-        </div>
-        {image && 
-          <div className="preViewImage">
-            <UilTimes style={{cursor: 'pointer', position: 'relative', zIndex: 1000}} onClick={() => setImage(null)} />
-            <img src={image.image} alt="" />
+    <div className="cp">
+      <div className="CreateProject">
+        <h1>Create New Project</h1>
+        <div className="inputs">
+          <div className="line1">
+            <input type="text" placeholder="Project Name" />
+            <AsyncSelect
+              value={selectedCompany}
+              styles={style}
+              placeholder={"Company"}
+              className="select"
+            ></AsyncSelect>
           </div>
-        }
+          <div className="line2">
+            <AsyncSelect
+              className="select-cw"
+              isMulti
+              styles={style}
+              value={selectedCoworkers}
+              placeholder={"Co-Workers"}
+            ></AsyncSelect>
+          </div>
+          <div className="line3">
+            <textarea
+              className="description"
+              cols="30"
+              rows="10"
+              placeholder="Description"
+            ></textarea>
+          </div>
+          <div className="line4" style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
+          <div className="addImage" onClick={() => imageRef.current.click()}>
+            <BsFillImageFill
+              style={{ fontSize: 30, position: "relative", zIndex: 1000 }}
+            />
+            <p>Add Photo</p>
+          </div>
+          {image && (
+            <div className="preViewImage">
+              <UilTimes
+                style={{
+                  cursor: "pointer",
+                  position: "relative",
+                  zIndex: 1000,
+                }}
+                onClick={() => setImage(null)}
+              />
+              <img src={image.image} alt="" />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
