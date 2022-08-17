8fbf3d909e198c2ceccac81e1137308fa2a6e694
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-17 12:42:37 +0430

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import CreateProject from "./Pages/CreateProject/CreateProject";
 import Home from "./Pages/Home/Home";
-import { UserProfile } from "./Pages/UserProfile/UserProfile";
 function App() {
   return (
     <div className="App">
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-      {/* <Home /> */}
-      <UserProfile />
+      <Home />
     </div>
   );
 }
