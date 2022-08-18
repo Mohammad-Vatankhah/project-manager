6cd608dda1f8ed1e72ea7bca73ca1cd1bbc40f7d
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-18 16:26:51 +0430

    modified

--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import AuthRoute from "./Routes/AuthRoute.js";
+
+// Router
+
 const app = express();
+
+// Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
@@ -13,5 +19,13 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => app.listen(process.env.Port, console.log(`Listening to port ${process.env.Port}`)))
+  .then(() =>
+    app.listen(
+      process.env.Port,
+      console.log(`Listening to port ${process.env.Port}`)
+    )
+  )
   .catch((error) => console.log(error.message));
+
+// usage of routes
+app.use("/auth", AuthRoute);
