0eaa452d891a898808714a3296f96651f47e0d34
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-18 12:24:41 +0430

    components added to company page

--- client/src/Pages/CompanyPage/CompanyPage.jsx
+++ client/src/Pages/CompanyPage/CompanyPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { CompanyPageLeft } from '../../Components/CompanyPageLeft/CompanyPageLeft'
-import "./CompanyPage.css"
+import React from "react";
+import { CompanyPageLeft } from "../../Components/CompanyPageLeft/CompanyPageLeft";
+import PostSide from "../../Components/PostSide/PostSide";
+import RightSide from "../../Components/RightSide/RightSide";
+import { CompanyDetails } from "../../Components/CompanyDetails/CompanyDetails";
+
+import "./CompanyPage.css";
 export const CompanyPage = () => {
   return (
     <div className="CompanyPage">
-        <CompanyPageLeft />
+      <CompanyPageLeft />
+      <div className="companyPageCenter">
+        <CompanyDetails />
+        <PostSide />
+      </div>
+      <RightSide />
     </div>
-    
-  )
-}
+  );
+};
