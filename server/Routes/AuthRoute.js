2053de2024570bbf152e4feacf79e5e703c9b2f4
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-18 16:27:27 +0430

    router for login and register user added

---
+++ server/Routes/AuthRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { loginUser, registerUser } from "../Controllers/AuthController.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+export default router;
