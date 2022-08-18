36f3f92ddcd8581477ab9f9fa70fe91bd30c8546
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-18 11:53:24 +0430

    company page created

---
+++ client/src/Pages/CompanyPage/CompanyPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CompanyPageLeft } from '../../Components/CompanyPageLeft/CompanyPageLeft'
+import "./CompanyPage.css"
+export const CompanyPage = () => {
+  return (
+    <div className="CompanyPage">
+        <CompanyPageLeft />
+    </div>
+    
+  )
+}
