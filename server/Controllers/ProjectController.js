ca7c7d2f04ec8e2e9606b8c488b8b01b3295d28f
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-19 15:38:59 +0430

    delete post function added

--- server/Controllers/ProjectController.js
+++ server/Controllers/ProjectController.js
@@ -34,9 +34,26 @@ export const updateProject = async (req, res) => {
     const project = await ProjectModel.findById(projectId);
     if (project.publisher === currentUserId) {
       await project.updateOne({ $set: req.body });
-      res.status(200).json("Project updated!")
+      res.status(200).json("Project updated!");
     } else {
-        res.status(403).json("Action forbidden.")
+      res.status(403).json("Action forbidden.");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// delete a project
+export const deleteProject = async (req, res) => {
+  const projectId = req.params.id;
+  const { currentUserId, currentUserAdminStatus } = req.body;
+  try {
+    const project = await ProjectModel.findById(projectId);
+    if (project.publisher === currentUserId || currentUserAdminStatus) {
+      await project.deleteOne();
+      res.status(200).json("Project deleted!");
+    } else {
+      res.status(403).json("Action forbidden.");
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
