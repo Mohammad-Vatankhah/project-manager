a9b25e7636a2f8f2ff40b40753bbaccc773f5981
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-19 13:00:25 +0430

    follow user route added

--- server/Routes/UserRout.js
+++ server/Routes/UserRout.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { deleteUser, getUser, updateUser } from "../Controllers/UserController.js";
+import { deleteUser, followUser, getUser, updateUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 router.get("/:id", getUser);
 router.put("/:id", updateUser)
 router.delete("/:id", deleteUser)
+router.put("/:id/follow", followUser)
 export default router;
