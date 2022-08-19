f30cb4d6d658e84ee4a19bcc2f92d3fdf887a17d
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-19 15:39:08 +0430

    delete post route added

--- server/Routes/ProjectRoute.js
+++ server/Routes/ProjectRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createProject, getProject, updateProject } from "../Controllers/ProjectController.js";
+import { createProject, deleteProject, getProject, updateProject } from "../Controllers/ProjectController.js";
 
 const router = express.Router();
 
 router.post("/createProject", createProject);
 router.get("/:id/getProject", getProject)
 router.put("/:id/updateProject", updateProject)
+router.delete('/:id/deleteProject', deleteProject)
 export default router;
