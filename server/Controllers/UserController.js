e1546c566b94a0a7154145b8379b6de6fe66f1ff
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-19 12:39:55 +0430

    delete user added to user controller

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -46,3 +46,21 @@ export const updateUser = async (req, res) => {
       .json("Access Denied! you can only update your own profile.");
   }
 };
+
+// Delete user
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currentUserAdminStatus } = req.body;
+  if (currentUserId === id || currentUserAdminStatus) {
+    try {
+      await UserModel.findByIdAndDelete(id)
+      res.status(200).json("User deleted successfully")
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    res
+      .status(403)
+      .json("Access Denied! you can only delete your own profile.");
+  }
+};
