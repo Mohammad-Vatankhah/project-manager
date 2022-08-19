d1786604433ad3ef01106108ff48be61ebab6a8d
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-19 13:58:38 +0430

    unfollow user route added

--- server/Routes/UserRout.js
+++ server/Routes/UserRout.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { deleteUser, followUser, getUser, updateUser } from "../Controllers/UserController.js";
+import { deleteUser, followUser, getUser, unfollowUser, updateUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 router.get("/:id", getUser);
 router.put("/:id", updateUser)
 router.delete("/:id", deleteUser)
 router.put("/:id/follow", followUser)
+router.put("/:id/unfollow", unfollowUser)
 export default router;
