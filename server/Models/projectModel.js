3436fcc09b5c2693702ca33e9592925b49612bba
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-19 14:31:34 +0430

    project model created

---
+++ server/Models/projectModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = mongoose.Schema(
+  {
+    publisher: {
+      type: String,
+      requred: true,
+    },
+    image: String,
+    desc: String,
+    likes: [],
+    employess: [],
+    process: [],
+    comments: [],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProjectModel = mongoose.model("Projects", ProjectSchema);
+export default UserModel;
