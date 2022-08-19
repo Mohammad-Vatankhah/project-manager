c415524b810dd69cedb4331655f313d232dff940
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-19 17:03:00 +0430

    process and comment schemas added

--- server/Models/projectModel.js
+++ server/Models/projectModel.js
@@ -1,4 +1,32 @@
 import mongoose from "mongoose";
+const ProcessSchema = mongoose.Schema(
+  {
+    employee: {
+      type: String,
+      required: true,
+    },
+    image: String,
+    desc: String,
+  },
+  { timestamps: true }
+);
+
+const CommentSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 
 const ProjectSchema = mongoose.Schema(
   {
@@ -8,15 +36,20 @@ const ProjectSchema = mongoose.Schema(
     },
     image: String,
     desc: String,
-    likes: [],
-    employess: [],
-    process: [],
-    comments: [],
+    likes: [String],
+    employees: {
+      type: [String],
+      required: true,
+    },
+    process: [ProcessSchema],
+    comments: [CommentSchema],
   },
   {
     timestamps: true,
   }
-);
-
-const ProjectModel = mongoose.model("Projects", ProjectSchema);
-export default ProjectModel;
+  );
+  
+  const ProjectModel = mongoose.model("Projects", ProjectSchema);
+  export default ProjectModel;
+  
+  
