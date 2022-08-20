55b466ddf15fd7d776719777fe6f25275ec6d8d9
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-20 14:24:41 +0430

    get company router added

--- server/Routes/CompanyRoute.js
+++ server/Routes/CompanyRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createCompany } from "../Controllers/CompanyController.js";
+import { createCompany, getCompany } from "../Controllers/CompanyController.js";
 
 const router = express.Router();
 
 router.post("/createCompany", createCompany);
+router.get("/:id/getCompany", getCompany);
 export default router;
