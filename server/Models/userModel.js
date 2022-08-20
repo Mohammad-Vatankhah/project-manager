432a3e174b5fe8e7a33c75f75e5fd62785d7a010
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-20 15:40:26 +0430

    Update userModel.js

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -5,6 +5,7 @@ const UserSchema = mongoose.Schema(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     firstName: {
       type: String,
@@ -17,6 +18,7 @@ const UserSchema = mongoose.Schema(
     Email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
