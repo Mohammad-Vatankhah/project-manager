8038c013ae6d778b63df9c0453389c174a743c31
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-20 14:48:18 +0430

    update company route added

--- server/Routes/CompanyRoute.js
+++ server/Routes/CompanyRoute.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createCompany, getCompany } from "../Controllers/CompanyController.js";
+import {
+  createCompany,
+  getCompany,
+  updateCompany,
+} from "../Controllers/CompanyController.js";
 
 const router = express.Router();
 
 router.post("/createCompany", createCompany);
 router.get("/:id/getCompany", getCompany);
+router.put("/:id/updateCompany", updateCompany);
 export default router;
