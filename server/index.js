2d5d1165b35ae6f6de1e157404b6d3e8dec79f9b
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-20 12:58:11 +0430

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import AuthRoute from "./Routes/AuthRoute.js";
-import UserRoute from "./Routes/UserRout.js";
+import UserRoute from "./Routes/UserRoute.js";
 import ProjectRoute from "./Routes/ProjectRoute.js";
 
 // Router
