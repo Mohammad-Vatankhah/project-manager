dab8b1c5d6e7f508125d99cbedf9faa0a5ceb0e9
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-22 16:42:50 +0430

    routes added for home and auth pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
-import Home from "./Pages/Home/Home";
+import { Auth } from "./Pages/Auth/Auth";
+import { Home } from "./Pages/Home/Home";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 function App() {
+  const user = useSelector((state) => state.authReducer.authData);
   return (
     <div className="App">
-      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      ّ<div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-      <Home />
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+      </Routes>
     </div>
   );
 }
