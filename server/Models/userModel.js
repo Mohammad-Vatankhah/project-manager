61bbd74ccc2685ba5463fb66b8dfb3b6a6557bdc
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-22 15:51:35 +0430

    Update userModel.js

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -4,25 +4,25 @@ const UserSchema = mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
-      unique: true,
+      required: [true, "Username required!"],
+      // unique: [true, "Username already taken"],
     },
     firstName: {
       type: String,
-      required: true,
+      required: [true, "First name required!"],
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, "last name required!"],
     },
     Email: {
       type: String,
       required: true,
-      unique: true,
+      // unique: [true, "There is already an account with this Email"],
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Password required!"],
     },
     status: String,
     livesIn: String,
