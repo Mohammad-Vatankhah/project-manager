6e8c86e9bf4e057c877f83a69ee3a1b7edfd20ee
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-22 12:05:16 +0430

    auth reducers created

---
+++ client/src/reducers/AuthReducers.js
@@ -0,0 +1,16 @@
+const authReducer = (
+  state = { authData: null, loading: false, error: false },
+  action
+) => {
+  switch (action.type) {
+    case "AUTH_START":
+      return { ...state, loading: true, error: false };
+    case "AUTH_SUCCESS":
+      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      return { ...state, authData: action.data, loading: false, error: false };
+    case "AUTH-FAIL":
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+};
