d543e3d5d64957c1e1e6067c84449b6c7482c4e7
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-22 15:49:15 +0430

    Update AuthRequest.js

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -2,10 +2,5 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
-export const login = (formData) => {
-  API.post("/auth/login", formData);
-};
-
-export const signup = (formData) => {
-    API.post("/auth/register", formData);
-  };
+export const login = (formData) => API.post("/auth/login", formData);
+export const signup = (formData) => API.post("/auth/register", formData);
