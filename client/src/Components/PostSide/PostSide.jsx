eab5183b24865f1b02a9c7baef8c0741b478ea9f
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-23 11:38:48 +0430

    create project link added

--- client/src/Components/PostSide/PostSide.jsx
+++ client/src/Components/PostSide/PostSide.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import Projects from '../Projects/Projects'
-import "./PostSide.css"
+import React from "react";
+import Projects from "../Projects/Projects";
+import "./PostSide.css";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const PostSide = () => {
+  const user = useSelector((state) => state.authReducer.authData.user);
   return (
-    <div className='PostSide'>
-        <button className='button' id='create-button'>Create new project</button>
-        <Projects />
+    <div className="PostSide">
+      <Link style={{ textDecoration: "none" }} to={`/createProject/${user._id}`}>
+        <button className="button" id="create-button">
+          Create new project
+        </button>
+      </Link>
+      <Projects />
     </div>
-  )
-}
+  );
+};
 
-export default PostSide
+export default PostSide;
