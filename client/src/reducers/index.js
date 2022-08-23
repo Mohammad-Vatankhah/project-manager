bce9144532599d3c42a49d4deea1f83bf247c457
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-23 20:40:04 +0430

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./AuthReducers";
+import companyReducer from "./CompanyReducers";
+import projectReducer from "./ProjectReducer";
 
-export const reducers = combineReducers({ authReducer });
+export const reducers = combineReducers({
+  authReducer,
+  companyReducer,
+  projectReducer,
+});
