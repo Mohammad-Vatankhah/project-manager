b434f401eb2d0318a4c805b1edc5667a14227098
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 15:02:00 +0430

    like logic added

--- client/src/Components/ProjectReaction/ProjectReaction.jsx
+++ client/src/Components/ProjectReaction/ProjectReaction.jsx
@@ -3,19 +3,26 @@ import "./ProjectReaction.css";
 import { AiOutlineHeart } from "react-icons/ai";
 import { AiFillHeart } from "react-icons/ai";
 import { useSelector } from "react-redux";
+import { likePost } from "../../api/ProjectRequest";
 
 export const ProjectReaction = (data) => {
-   const data1 = data.data
-   const user = useSelector((state) => state.authReducer.authData.user);
+  const data1 = data.data;
+  const user = useSelector((state) => state.authReducer.authData.user);
   const likeStyle = { fontSize: 30, color: "var(--blue)", cursor: "pointer" };
   const [liked, setLiked] = useState(data1.data.likes.includes(user._id));
   const [likes, setLikes] = useState(data1.data.likes.length);
+  const handleLike = () => {
+    setLiked((prev) => !prev);
+    console.log(user._id);
+    likePost(data1.data._id, user._id);
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+  };
   return (
     <div className="ProjectReaction">
       {liked ? (
-        <AiFillHeart style={likeStyle} />
+        <AiFillHeart style={likeStyle} onClick={handleLike} />
       ) : (
-        <AiOutlineHeart style={likeStyle} />
+        <AiOutlineHeart style={likeStyle} onClick={handleLike} />
       )}
       <span
         style={{
