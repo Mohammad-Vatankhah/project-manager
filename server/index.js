92c8c4f446ef0027ef491451bab55c31ea4f0625
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 14:25:38 +0430

    public folder is accessible now

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ import UploadRoute from "./Routes/UploadRoute.js";
 
 const app = express();
 
+// make images folder accessible
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
