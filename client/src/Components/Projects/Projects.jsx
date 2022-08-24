fe5b7db1a9de0147edcc45d9f522f71f628ea69a
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 14:24:51 +0430

    retrieving projects from server handle

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import "./Projects.css"
-import Project from '../Project/Project'
-import { ProjectData } from '../../Data/ProjectData'
+import React from "react";
+import "./Projects.css";
+import Project from "../Project/Project";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getTimelinePost } from "../../actions/ProjectAction";
 const Projects = () => {
+  useEffect(() => {
+    dispatch(getTimelinePost(user._id));
+  }, []);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.authReducer.authData.user);
+  const { project, loading } = useSelector((state) => state.projectReducer);
   return (
-    <div className='Projects'>
-      {ProjectData.map((project,id) => {
-        return <Project data={project} id={id}/>
-      })}
+    <div className="Projects">
+      {loading
+        ? "Loading projects. Please wait..."
+        : project.map((project, id) => {
+            return <Project data={project} id={id} />;
+          })}
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
