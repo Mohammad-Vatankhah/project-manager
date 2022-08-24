64ed9a1600a3d4c0b9a033d068650e5fac4353fd
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 15:43:05 +0430

    Update RightSide.jsx

--- client/src/Components/RightSide/RightSide.jsx
+++ client/src/Components/RightSide/RightSide.jsx
@@ -6,22 +6,14 @@ import { MdNotificationsNone } from "react-icons/md";
 import { BiMessageDetail } from "react-icons/bi";
 import TrendCard from "../TrendCard/TrendCard";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Home } from "../../Pages/Home/Home";
 
 const RightSide = () => {
-  const user = useSelector((state) => state.authReducer.authData);
   const iconStyle = { fontSize: 30, cursor: "pointer" };
-  const iconStyleActive = {
-    fontSize: 30,
-    cursor: "pointer",
-    color: "var(--blue)",
-  };
   return (
     <div className="RightSide">
       <div className="navIcons">
         <Link style={{ textDecoration: "none" }} to="../home">
-          <FiHome style={iconStyleActive} />
+          <FiHome style={iconStyle} />
         </Link>
         <FiSettings style={iconStyle} />
         <MdNotificationsNone style={iconStyle} />
