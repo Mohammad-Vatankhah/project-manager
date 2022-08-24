4ec1ea6766af6bbcb2b81595f288d511b1577bdc
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 16:15:52 +0430

    Update UserProfile.jsx

--- client/src/Pages/UserProfile/UserProfile.jsx
+++ client/src/Pages/UserProfile/UserProfile.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { UserProfileLeft } from "../../Components/UserProfileLeft/UserProfileLeft";
 import "./UserProfile.css";
-import ProfileCard from "../../Components/ProfileCard/ProfileCard"
-import Projects from "../../Components/Projects/Projects"
-import RightSide from "../../Components/RightSide/RightSide"
-
+import ProfileCard from "../../Components/ProfileCard/ProfileCard";
+import Projects from "../../Components/Projects/Projects";
+import RightSide from "../../Components/RightSide/RightSide";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 export const UserProfile = () => {
-  return <div className="UserProfile">
-        <UserProfileLeft />
-        <div className="profileCenter">
-            <ProfileCard />
-            <Projects />
-        </div>
-            <RightSide />
-      </div>;
+  const user = useSelector((state) => state.authReducer.authData.user);
+  return (
+    <div className="UserProfile">
+      <UserProfileLeft />
+      <div className="profileCenter">
+        <ProfileCard location="profilePage" />
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`/createProject/${user._id}`}
+        >
+          <button className="button" id="create-button">
+            Create new project
+          </button>
+        </Link>
+        <Projects location={"userProfile"} />
+      </div>
+      <RightSide />
+    </div>
+  );
 };
