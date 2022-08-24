e8d2e01e923bf18c7090fe91d7808108dc146a14
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 16:15:09 +0430

    fixed for profile page

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -4,13 +4,15 @@ import Project from "../Project/Project";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getTimelinePost } from "../../actions/ProjectAction";
-const Projects = () => {
+const Projects = ({ location }) => {
   useEffect(() => {
     dispatch(getTimelinePost(user._id));
   }, []);
   const dispatch = useDispatch();
   const user = useSelector((state) => state.authReducer.authData.user);
   const { project, loading } = useSelector((state) => state.projectReducer);
+  location === "userProfile" &&
+    project.filter((project) => project.publisher === user._id);
   return (
     <div className="Projects">
       {loading
