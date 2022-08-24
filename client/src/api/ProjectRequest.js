7b477e10c21fc6c021b01757a4ab08074efb7581
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 15:01:44 +0430

    like post request added

--- client/src/api/ProjectRequest.js
+++ client/src/api/ProjectRequest.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const getTimelinePost = (id) => API.get(`/project/${id}/timeLine`);
+export const likePost = (id, currentUserId) =>
+  API.put(`project/${id}/likeProject`, { currentUserId });
