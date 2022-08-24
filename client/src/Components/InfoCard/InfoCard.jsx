970016561ac4846c44b322db054b36594d2818b8
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-24 17:04:11 +0430

    info card is dynamic now

--- client/src/Components/InfoCard/InfoCard.jsx
+++ client/src/Components/InfoCard/InfoCard.jsx
@@ -2,17 +2,51 @@ import React, { useState } from "react";
 import "./InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
 import ProfileModal from "../ProfileModal/ProfileModal";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import * as UserApi from "../../api/UserRequest.js";
+import { logOut } from "../../actions/AuthActions";
 export const InfoCard = () => {
   const [modalOpened, setModalOpened] = useState(false);
+  const dispatch = useDispatch();
+  const params = useParams();
+  const profileUserId = params.id;
+  const [profileUser, SetProfileUser] = useState({});
+
+  const user = useSelector((state) => state.authReducer.authData.user);
+  useEffect(() => {
+    const fetchProfileUser = async () => {
+      if (profileUserId === user._id) {
+        SetProfileUser(user);
+      } else {
+        const profileUser = await UserApi.getUser(profileUserId);
+        SetProfileUser(profileUser);
+        console.log(profileUser);
+      }
+    };
+    fetchProfileUser();
+  }, [user]);
+
+  const handleLogOut = () => {
+    dispatch(logOut());
+  };
   return (
     <div className="InfoCard">
       <div className="infoHead">
-        <h4>Your Info</h4>
-        <UilPen
-          width="2rem"
-          height="1.2rem"
-          onClick={() => setModalOpened(true)}
-        />
+        {profileUserId === user._id ? (
+          <>
+            <h4>Your Info</h4>
+            <UilPen
+              width="2rem"
+              height="1.2rem"
+              onClick={() => setModalOpened(true)}
+            />
+          </>
+        ) : (
+          <h4>{profileUser.name + "'s info"}</h4>
+        )}
+
         <ProfileModal
           modalOpened={modalOpened}
           setModalOpened={setModalOpened}
@@ -22,27 +56,21 @@ export const InfoCard = () => {
         <span>
           <b>Status: </b>
         </span>
-        <span>Learning</span>
+        <span>{profileUser.status}</span>
       </div>
       <div className="info">
         <span>
           <b>Lives in: </b>
         </span>
-        <span>Ardakan</span>
-      </div>
-      <div className="info">
-        <span>
-          <b>Company: </b>
-        </span>
-        <span>No where</span>
+        <span>{profileUser.livesIn}</span>
       </div>
       <div className="info">
         <span>
           <b>E-mail: </b>
         </span>
-        <span>example@example.com</span>
+        <span>{profileUser.Email}</span>
       </div>
-      <button className="button" id="logout-button">
+      <button className="button" id="logout-button" onClick={handleLogOut}>
         Log Out
       </button>
     </div>
