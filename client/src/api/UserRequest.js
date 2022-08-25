6c59c84bbe2df9d464b22b37edb9ed2ca8a2fd41
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-25 11:39:30 +0430

    update user request added

--- client/src/api/UserRequest.js
+++ client/src/api/UserRequest.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const getUser = (id) => API.get(`/user/${id}`);
+
+export const updateUser = (id, formData) => API.put(`/user/${id}/update`, formData);
