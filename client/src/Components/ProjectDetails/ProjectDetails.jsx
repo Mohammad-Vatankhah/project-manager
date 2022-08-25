473bf5b28acb6bc7fdd8df33d11600e8fdabfc1d
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-25 16:48:42 +0430

    a bug fixed

--- client/src/Components/ProjectDetails/ProjectDetails.jsx
+++ client/src/Components/ProjectDetails/ProjectDetails.jsx
@@ -4,11 +4,10 @@ import { useSelector } from "react-redux";
 export const ProjectDetails = (props) => {
   const { data, img } = props;
   const style = img ? { marginTop: "15px" } : { marginBottom: "15px" };
-  const user = useSelector((state) => state.authReducer.authData.user);
   return (
     <div className="ProjectDetails" style={style}>
       <span>
-        <b>{user.username}:</b>
+        <b>{data.data.employees[0]}:</b>
       </span>
       <span> {data.data.desc}</span>
     </div>
