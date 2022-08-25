b03a795c5ba51ee4a2c396e1854c68fac0d15398
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-25 11:40:14 +0430

    update reducers added

--- client/src/reducers/AuthReducers.js
+++ client/src/reducers/AuthReducers.js
@@ -10,6 +10,13 @@ const authReducer = (
       return { ...state, authData: action.data, loading: false, error: false };
     case "AUTH_FAIL":
       return { ...state, loading: false, error: true };
+    case "UPDATING_START":
+      return { ...state, loading: false, error: false };
+    case "UPDATING_SUCCESS":
+      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      return { ...state, authData: action.data, loading: false, error: false };
+    case "UPDATING_FAIL":
+      return { ...state, loading: false, error: true };
     case "LOG_OUT":
       localStorage.clear();
       return { ...state, authData: null, loading: false, error: false };
