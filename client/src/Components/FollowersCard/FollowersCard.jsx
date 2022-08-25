d7f1e6769b7e2634879ea9f63717124c7162d708
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-25 13:00:28 +0430

    followers card is dynamic now

--- client/src/Components/FollowersCard/FollowersCard.jsx
+++ client/src/Components/FollowersCard/FollowersCard.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./FollowersCard.css";
 
-import { Followers } from "../../Data/FollowersData";
+import { User } from "../User/User";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { getAllUser } from "../../api/UserRequest";
 const FollowersCard = () => {
+  const [people, setPeople] = useState([]);
+  const user = useSelector((state) => state.authReducer.authData.user);
+  useEffect(() => {
+    const fetchPeople = async () => {
+      const { data } = await getAllUser();
+      setPeople(data);
+    };
+    fetchPeople();
+  }, []);
   return (
     <div className="FollowersCard">
-      <h3>Who is following you</h3>
-      {Followers.map((follower, id) => {
-        return (
-          <div className="follower">
-            <div>
-              <img src={follower.img} alt="" className="followerImage" />
-              <div className="name">
-                <span>{follower.name}</span>
-                <span>@{follower.username}</span>
-              </div>
-            </div>
-            <button className="button" id="fc-button">Follow</button>
-          </div>
-        );
-      })}
+      {people.length > 0 && (
+        <>
+          <h3>People you may know</h3>
+          {people.map((person, id) => {
+            if (person._id !== user._id) {
+              console.log(person);
+              return (
+                <div className="follower">
+                  <User person={person} key={id} />
+                </div>
+              );
+            }
+          })}
+        </>
+      )}
     </div>
   );
 };
