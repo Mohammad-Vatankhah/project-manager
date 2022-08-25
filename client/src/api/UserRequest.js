4d4f31cd0403e695f94cb87224059b943a4d5b5a
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-25 17:21:44 +0430

    Authorization token added to request headers

--- client/src/api/UserRequest.js
+++ client/src/api/UserRequest.js
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
 export const getUser = (id) => API.get(`/user/${id}`);
 
 export const updateUser = (id, formData) =>
