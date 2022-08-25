c927bf5c088748e5c5718ffc2edd9cf92e6a7d2c
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-25 13:01:46 +0430

    get all users route added

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { deleteUser, followUser, getUser, unfollowUser, updateUser } from "../Controllers/UserController.js";
+import {
+  deleteUser,
+  followUser,
+  getAllUsers,
+  getUser,
+  unfollowUser,
+  updateUser,
+} from "../Controllers/UserController.js";
 
 const router = express.Router();
 router.get("/:id/get", getUser);
-router.put("/:id/update", updateUser)
-router.delete("/:id/delete", deleteUser)
-router.put("/:id/follow", followUser)
-router.put("/:id/unfollow", unfollowUser)
+router.put("/:id/update", updateUser);
+router.delete("/:id/delete", deleteUser);
+router.put("/:id/follow", followUser);
+router.put("/:id/unfollow", unfollowUser);
+router.get("/allUsers", getAllUsers);
 export default router;
