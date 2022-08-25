a780b574d7afda97be33b40c0765aa6a9901ead9
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-25 16:49:04 +0430

    fixed for user profile page

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -4,15 +4,20 @@ import Project from "../Project/Project";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getTimelinePost } from "../../actions/ProjectAction";
-const Projects = ({ location }) => {
+import { useParams } from "react-router-dom";
+const Projects = () => {
+  const params = useParams();
   useEffect(() => {
     dispatch(getTimelinePost(user._id));
   }, []);
   const dispatch = useDispatch();
   const user = useSelector((state) => state.authReducer.authData.user);
-  const { project, loading } = useSelector((state) => state.projectReducer);
-  location === "userProfile" &&
-    project.filter((project) => project.publisher === user._id);
+  let { project, loading } = useSelector((state) => state.projectReducer);
+  if (!project) {
+    return "No projects available";
+  }
+  if (params.id)
+    project = project.filter((project) => project.publisher === params.id);
   return (
     <div className="Projects">
       {loading
