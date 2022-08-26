49d137bb08fc188ec3f66c63617a6600d30f8b03
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-26 12:51:58 +0430

    Update InfoCard.jsx

--- client/src/Components/InfoCard/InfoCard.jsx
+++ client/src/Components/InfoCard/InfoCard.jsx
@@ -3,38 +3,19 @@ import "./InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
 import ProfileModal from "../ProfileModal/ProfileModal";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-import * as UserApi from "../../api/UserRequest.js";
 import { logOut } from "../../actions/AuthActions";
-export const InfoCard = () => {
+export const InfoCard = ({ user }) => {
   const [modalOpened, setModalOpened] = useState(false);
   const dispatch = useDispatch();
-  const params = useParams();
-  const profileUserId = params.id;
-  const [profileUser, SetProfileUser] = useState({});
 
-  const user = useSelector((state) => state.authReducer.authData.user);
-  useEffect(() => {
-    const fetchProfileUser = async () => {
-      if (profileUserId === user._id) {
-        SetProfileUser(user);
-      } else {
-        const profileUser = await UserApi.getUser(profileUserId);
-        SetProfileUser(profileUser);
-        console.log(profileUser);
-      }
-    };
-    fetchProfileUser();
-  }, [user]);
-
+  const user1 = useSelector((state) => state.authReducer.authData.user);
   const handleLogOut = () => {
     dispatch(logOut());
   };
   return (
     <div className="InfoCard">
       <div className="infoHead">
-        {profileUserId === user._id ? (
+        {user._id === user1._id ? (
           <>
             <h4>Your Info</h4>
             <UilPen
@@ -44,36 +25,38 @@ export const InfoCard = () => {
             />
           </>
         ) : (
-          <h4>{profileUser.name + "'s info"}</h4>
+          <h4>{user.firstName + "'s info"}</h4>
         )}
 
         <ProfileModal
           modalOpened={modalOpened}
           setModalOpened={setModalOpened}
-          data = {user}
+          data={user}
         />
       </div>
       <div className="info">
         <span>
           <b>Status: </b>
         </span>
-        <span>{profileUser.status}</span>
+        <span>{user.status}</span>
       </div>
       <div className="info">
         <span>
           <b>Lives in: </b>
         </span>
-        <span>{profileUser.livesIn}</span>
+        <span>{user.livesIn}</span>
       </div>
       <div className="info">
         <span>
           <b>E-mail: </b>
         </span>
-        <span>{profileUser.Email}</span>
+        <span>{user.Email}</span>
       </div>
-      <button className="button" id="logout-button" onClick={handleLogOut}>
-        Log Out
-      </button>
+      {user._id === user1._id && (
+        <button className="button" id="logout-button" onClick={handleLogOut}>
+          Log Out
+        </button>
+      )}
     </div>
   );
 };
