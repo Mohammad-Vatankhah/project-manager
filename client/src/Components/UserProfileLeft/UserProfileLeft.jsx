2e3c6bc36212e0f5f5b0b2a4f8b3ce201ee21be4
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-26 12:52:47 +0430

    Update UserProfileLeft.jsx

--- client/src/Components/UserProfileLeft/UserProfileLeft.jsx
+++ client/src/Components/UserProfileLeft/UserProfileLeft.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import LogoSearch from "../LogoSearch/LogoSearch"
-import FollowersCard from "../FollowersCard/FollowersCard"
-import { InfoCard } from '../InfoCard/InfoCard'
-import "../ProfileSide/ProfileSide.css"
-import { CompaniesCard } from '../CompaniesCard/CompaniesCard'
-export const UserProfileLeft = () => {
+import React from "react";
+import LogoSearch from "../LogoSearch/LogoSearch";
+import FollowersCard from "../FollowersCard/FollowersCard";
+import { InfoCard } from "../InfoCard/InfoCard";
+import "../ProfileSide/ProfileSide.css";
+import { CompaniesCard } from "../CompaniesCard/CompaniesCard";
+export const UserProfileLeft = ({ user }) => {
   return (
-    <div className='ProfileSide'>
-        <LogoSearch />
-        <InfoCard />
-        <CompaniesCard />
-        <FollowersCard />
+    <div className="ProfileSide">
+      <LogoSearch />
+      <InfoCard user={user} />
+      <CompaniesCard />
+      <FollowersCard />
     </div>
-  )
-}
+  );
+};
