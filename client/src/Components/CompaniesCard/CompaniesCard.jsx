42d34bf70a01fb2d28862233f628315d0998e9eb
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-26 15:40:14 +0430

    companies card is dynamic now

--- client/src/Components/CompaniesCard/CompaniesCard.jsx
+++ client/src/Components/CompaniesCard/CompaniesCard.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import "./CompaniesCard.css";
-import { Companies } from "../../Data/CompaniesData";
+import * as CompanyApi from "../../api/CompanyRequests";
 import { useState } from "react";
 import { CompanyModal } from "../CompanyModal/CompanyModal";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 export const CompaniesCard = () => {
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const params = useParams();
+  const profileUserId = params.id;
+  const user = useSelector((state) => state.authReducer.authData.user);
   const [modalOpened, setModalOpened] = useState(false);
+  const [companies, setCompanies] = useState([]);
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      const data = await CompanyApi.getUserCompanies(profileUserId);
+      setCompanies(data.data);
+    };
+    fetchCompanies();
+  }, []);
   return (
     <div className="CompaniesCard">
-      <h3>Your Companies</h3>
-      {Companies.map((company) => {
+      <h3>Companies</h3>
+      {companies.map((company) => {
         return (
-          <div className="company">
+          <div className="company" key={company._id}>
             <div>
-              <img src={company.img} alt="" className="companyImg" />
+              <img
+                src={
+                  company.profilePicture
+                    ? serverPublic + user.profilePicture
+                    : serverPublic + "defaultCompanyProfile.png"
+                }
+                alt=""
+                className="companyImg"
+              />
               <div className="name">
                 <span>{company.name}</span>
-                <span>{company.location}</span>
+                <span>{company.address}</span>
               </div>
             </div>
             <button className="button" id="view-button">
@@ -24,9 +47,16 @@ export const CompaniesCard = () => {
           </div>
         );
       })}
-      <button onClick={() => setModalOpened(true)} style={{ width: "100%", height: "2rem" }} className="button">
-        Create New Company
-      </button>
+      {user._id === profileUserId && (
+        <button
+          onClick={() => setModalOpened(true)}
+          style={{ width: "100%", height: "2rem" }}
+          className="button"
+        >
+          Create New Company
+        </button>
+      )}
+
       <CompanyModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
     </div>
   );
