fd0b1a6f23d3ec62c97fd98dd8bed4c8edfee8ff
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-26 14:27:35 +0430

    Update UserProfile.jsx

--- client/src/Pages/UserProfile/UserProfile.jsx
+++ client/src/Pages/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { UserProfileLeft } from "../../Components/UserProfileLeft/UserProfileLeft";
 import "./UserProfile.css";
 import ProfileCard from "../../Components/ProfileCard/ProfileCard";
@@ -6,49 +6,29 @@ import Projects from "../../Components/Projects/Projects";
 import RightSide from "../../Components/RightSide/RightSide";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-import * as UserApi from "../../api/UserRequest.js";
 
 export const UserProfile = () => {
   const user = useSelector((state) => state.authReducer.authData.user);
-  const params = useParams();
-  const profileUserId = params.id;
-  const [profileUser, SetProfileUser] = useState({});
-  useEffect(() => {
-    const fetchProfileUser = async () => {
-      if (profileUserId === user._id) {
-        SetProfileUser(user);
-      } else {
-        const profileUser = await UserApi.getUser(profileUserId);
-        SetProfileUser(profileUser.data);
-      }
-    };
-    fetchProfileUser();
-  }, []);
   return (
     <>
-      {profileUser && (
-        <div className="UserProfile">
-          <UserProfileLeft user={profileUser} />
-          <div className="profileCenter">
-            <ProfileCard
-              location="profilePage"
-              user={user}
-            />
-            <Link
-              style={{ textDecoration: "none" }}
-              to={`/createProject/${user._id}`}
-            >
-              <button className="button" id="create-button">
-                Create new project
-              </button>
-            </Link>
-            <Projects />
-          </div>
-          <RightSide />
+      (
+      <div className="UserProfile">
+        <UserProfileLeft />
+        <div className="profileCenter">
+          <ProfileCard location="profilePage"/>
+          <Link
+            style={{ textDecoration: "none" }}
+            to={`/createProject/${user._id}`}
+          >
+            <button className="button" id="create-button">
+              Create new project
+            </button>
+          </Link>
+          <Projects />
         </div>
-      )}
+        <RightSide />
+      </div>
+      )
     </>
   );
 };
