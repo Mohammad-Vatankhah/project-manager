3b404e3d287b409484e45a932381d5f31550695b
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-27 13:35:25 +0430

    Update CompanyActions.js

--- client/src/actions/CompanyActions.js
+++ client/src/actions/CompanyActions.js
@@ -15,8 +15,7 @@ export const createCompany = (data) => async (dispatch) => {
   dispatch({ type: "CREATE_START" });
   try {
     const company = await CompanyApi.createCompany(data);
-    console.log(company);
-    dispatch({ type: "CREATE_SUCCESS", data: company });
+    dispatch({ type: "CREATE_SUCCESS", data: company.data });
   } catch (error) {
     console.log(error);
     dispatch({ type: "CREATE_FAIL" });
