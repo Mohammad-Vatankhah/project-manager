8e7994ff062b48dad1bf7794abcd3196f3fb0730
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-27 12:04:01 +0430

    create company request added

--- client/src/api/CompanyRequests.js
+++ client/src/api/CompanyRequests.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
-export const getUserCompanies = (id) => API.get(`/company/${id}/getUserCompanies`);
+export const getUserCompanies = (id) =>
+  API.get(`/company/${id}/getUserCompanies`);
+
+export const createCompany = (formData) =>
+  API.post("company/createCompany", formData);
