7b12d8f5595b350a09886378c58d222a6d1ff108
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-27 12:46:47 +0430

    company actions added

---
+++ client/src/actions/CompanyActions.js
@@ -0,0 +1,24 @@
+import * as CompanyApi from "../api/CompanyRequests";
+
+export const getUserCompanies = (id) => async (dispatch) => {
+  dispatch({ type: "COMPANY_START" });
+  try {
+    const { data } = await CompanyApi.getUserCompanies(id);
+    dispatch({ type: "COMPANY_SUCCESS", data: data });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: "COMPANY_FAIL" });
+  }
+};
+
+export const createCompany = (data) => async (dispatch) => {
+  dispatch({ type: "CREATE_START" });
+  try {
+    const company = await CompanyApi.createCompany(data);
+    console.log(company);
+    dispatch({ type: "CREATE_SUCCESS", data: company });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: "CREATE_FAIL" });
+  }
+};
