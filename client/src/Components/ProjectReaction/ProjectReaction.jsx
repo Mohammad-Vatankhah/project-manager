29363dc1d03f33dc94ff54b9d9dfefd3087473f8
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-28 10:26:50 +0430

    Update ProjectReaction.jsx

--- client/src/Components/ProjectReaction/ProjectReaction.jsx
+++ client/src/Components/ProjectReaction/ProjectReaction.jsx
@@ -4,17 +4,22 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { AiFillHeart } from "react-icons/ai";
 import { useSelector } from "react-redux";
 import { likePost } from "../../api/ProjectRequest";
+import { useNavigate } from "react-router-dom";
 
-export const ProjectReaction = (data) => {
-  const data1 = data.data;
+export const ProjectReaction = ({ data, location }) => {
+  console.log(data);
+  const navigate = useNavigate();
   const user = useSelector((state) => state.authReducer.authData.user);
   const likeStyle = { fontSize: 30, color: "var(--blue)", cursor: "pointer" };
-  const [liked, setLiked] = useState(data1.data.likes.includes(user._id));
-  const [likes, setLikes] = useState(data1.data.likes.length);
+  const [liked, setLiked] = useState(data.likes.includes(user._id));
+  const [likes, setLikes] = useState(data.likes.length);
   const handleLike = () => {
     setLiked((prev) => !prev);
-    likePost(data1.data._id, user._id);
+    likePost(data._id, user._id);
     liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+  };
+  const handleView = () => {
+    navigate(`/project/${data._id}`);
   };
   return (
     <div className="ProjectReaction">
@@ -32,12 +37,15 @@ export const ProjectReaction = (data) => {
       >
         {likes} likes
       </span>
-      <button
-        className="button"
-        style={{ width: "6rem", height: "2rem", marginLeft: "15px" }}
-      >
-        View Project
-      </button>
+      {location === "home" && (
+        <button
+          className="button"
+          style={{ width: "6rem", height: "2rem", marginLeft: "15px" }}
+          onClick={handleView}
+        >
+          View Project
+        </button>
+      )}
     </div>
   );
 };
