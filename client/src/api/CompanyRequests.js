d97924210e578fd7c03c5069a3b1767601f6b803
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-28 13:02:29 +0430

    get company by username request added

--- client/src/api/CompanyRequests.js
+++ client/src/api/CompanyRequests.js
@@ -6,4 +6,7 @@ export const getUserCompanies = (id) =>
   API.get(`/company/${id}/getUserCompanies`);
 
 export const createCompany = (formData) =>
-  API.post("company/createCompany", formData);
+  API.post("/company/createCompany", formData);
+
+export const getCompanyByUsername = (username) =>
+  API.get(`/company/${username}/getCompanyByUsername`);
