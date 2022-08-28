ddc5cc49202ea2758d78181f427eb827f6894428
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-28 13:03:34 +0430

    get company by id function added

--- server/Controllers/CompanyController.js
+++ server/Controllers/CompanyController.js
@@ -6,8 +6,6 @@ export const createCompany = async (req, res) => {
   const company = new CompanyModel(req.body);
   try {
     await company.save();
-    const owner = await UserModel.findById(req.body.owner);
-    await owner.updateOne({ $push: { companies: company.companyId } });
     res.status(200).json(company);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -19,6 +17,21 @@ export const getCompany = async (req, res) => {
   const id = req.params.id;
   try {
     const company = await CompanyModel.findById(id);
+    if (company) {
+      res.status(200).json(company);
+    } else {
+      res.status(404).json("No such company exists!");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// get company by id
+export const getCompanyById = async (req, res) => {
+  const username = req.params.username;
+  try {
+    const company = await CompanyModel.findOne({ companyId: username });
     if (company) {
       res.status(200).json(company);
     } else {
