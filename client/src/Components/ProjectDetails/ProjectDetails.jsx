2cd2b4076d9332cc62c52180b9d932d8aed6ecf6
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-28 10:26:22 +0430

    Update ProjectDetails.jsx

--- client/src/Components/ProjectDetails/ProjectDetails.jsx
+++ client/src/Components/ProjectDetails/ProjectDetails.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-export const ProjectDetails = (props) => {
-  const { data, img } = props;
+export const ProjectDetails = ({data, img}) => {
   const style = img ? { marginTop: "15px" } : { marginBottom: "15px" };
   return (
     <div className="ProjectDetails" style={style}>
       <span>
-        <b>{data.data.employees[0]}:</b>
+        <b>{data.employees[0]}:</b>
       </span>
-      <span> {data.data.desc}</span>
+      <span> {data.desc}</span>
     </div>
   );
 };
