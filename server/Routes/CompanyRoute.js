8f5dbb8727240ab9932930010e4effb3bf03840c
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-28 13:03:59 +0430

    Update CompanyRoute.js

--- server/Routes/CompanyRoute.js
+++ server/Routes/CompanyRoute.js
@@ -4,6 +4,7 @@ import {
   createCompany,
   deleteCompany,
   getCompany,
+  getCompanyById,
   getUserCompanies,
   removeEmployee,
   updateCompany,
@@ -18,4 +19,5 @@ router.delete("/:id/deleteCompany", deleteCompany);
 router.put("/:id/addEmployee", addEmployee);
 router.put("/:id/removeEmployee", removeEmployee);
 router.get("/:id/getUserCompanies", getUserCompanies);
+router.get("/:username/getCompanyByUsername", getCompanyById);
 export default router;
