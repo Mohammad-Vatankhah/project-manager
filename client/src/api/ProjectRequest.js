3beffd257364e77cc4dddbebb6fbf5e5b83518c4
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-29 15:03:40 +0430

    add comment request added

--- client/src/api/ProjectRequest.js
+++ client/src/api/ProjectRequest.js
@@ -5,3 +5,8 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 export const getTimelinePost = (id) => API.get(`/project/${id}/timeLine`);
 export const likePost = (id, currentUserId) =>
   API.put(`project/${id}/likeProject`, { currentUserId });
+
+export const addComment = (id, data) =>
+  API.put(`/project/${id}/addComment`, data);
+
+export const getProject = (id) => API.get(`/project/${id}/getProject`);
