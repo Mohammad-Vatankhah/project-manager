2e3d28831502da472beddea9cb03df39e83c5bd7
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-29 15:03:58 +0430

    modified

--- client/src/Components/Comments/Comments.jsx
+++ client/src/Components/Comments/Comments.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Comments.css";
+import { BiSend } from "react-icons/bi";
+import * as ProjectApi from "../../api/ProjectRequest";
 import { Comment } from "../Comment/Comment";
-export const Comments = ({comments}) => {
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export const Comments = ({ project }) => {
+  const comment = useRef();
+  const [currentProject, setCorrentProject] = useState(project);
+  const user = useSelector((state) => state.authReducer.authData.user);
+  const handleSend = async () => {
+    comment.current.value !== "" &&
+      (await ProjectApi.addComment(project._id, {
+        userId: user._id,
+        desc: comment.current.value,
+      }));
+    comment.current.value = "";
+    setProject();
+  };
+  const setProject = () => {
+    ProjectApi.getProject(project._id).then((res) => {
+      setCorrentProject(res.data);
+    });
+  };
+
+  useEffect(() => {
+    setProject();
+  }, []);
   return (
     <div className="Comments">
-
-      {comments.map((comment) => {
-        return <Comment data={comment} key={comment._id}/>;
-      })}
+      <div className="create-comment">
+        <input type="text" placeholder="Post a comment" ref={comment} />
+        <BiSend
+          style={{ fontSize: "33px", color: "var(--blue)" }}
+          onClick={handleSend}
+        />
+      </div>
+      <div className="commentBox">
+        {currentProject.comments
+          .sort((a, b) => {
+            const keyA = new Date(a.createdAt);
+            const keyB = new Date(b.createdAt);
+            if (keyA < keyB) return 1;
+            if (keyA > keyB) return -1;
+            return 0;
+          })
+          .map((comment) => {
+            return <Comment data={comment} key={comment._id} />;
+          })}
+      </div>
     </div>
   );
 };
