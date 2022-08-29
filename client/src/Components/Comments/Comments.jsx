a537571fd1c357e04b38834201d02c56552ce1af
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-29 11:14:12 +0430

    Update Comments.jsx

--- client/src/Components/Comments/Comments.jsx
+++ client/src/Components/Comments/Comments.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Comments.css";
-import { CommentData } from "../../Data/CommentsData";
 import { Comment } from "../Comment/Comment";
-export const Comments = () => {
+export const Comments = ({comments}) => {
   return (
     <div className="Comments">
 
-      {CommentData.map((comment) => {
-        return <Comment data={comment} />;
+      {comments.map((comment) => {
+        return <Comment data={comment} key={comment._id}/>;
       })}
     </div>
   );
