a8314da5e03be29579abea4f63693cf5f4a1cfe9
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-29 11:14:07 +0430

    comments are dynamic now

--- client/src/Components/Comment/Comment.jsx
+++ client/src/Components/Comment/Comment.jsx
@@ -1,16 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import * as UserApi from "../../api/UserRequest";
 import "./Comment.css";
 export const Comment = ({ data }) => {
+  const [user, setUser] = useState({});
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const fetchUser = () => {
+    UserApi.getUser(data.userId).then((res) => {
+      const userData = res.data;
+      setUser(userData);
+    });
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <div className="Comment">
       <div className="comment-data">
-        <img src={data.profileImg} alt="" />
+        <img
+          src={
+            user.profilePicture
+              ? serverPublic + "defaultProfilePicture.jpg"
+              : serverPublic + user.profilePicture
+          }
+          alt=""
+        />
         <span className="cm">
-          <b>{data.name}:</b> 
-        </span><br />
-        <span>{data.comment}</span>
+          <b>{user.username}:</b>
+        </span>
+        <br />
+        <span>{data.desc}</span>
       </div>
-        <span style={{ color: "var(--gray)", fontSize: "12px",}}>{data.date}</span>
+      <span style={{ color: "var(--gray)", fontSize: "12px" }}>
+        {data.date}
+      </span>
     </div>
   );
 };
