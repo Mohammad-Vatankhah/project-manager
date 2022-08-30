5b1418dc7df8ce6e91a22899434944ae165c4e92
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-30 17:48:44 +0430

    process is dynamic now

--- client/src/Components/Process/Process.js
+++ client/src/Components/Process/Process.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Process.css";
-
-export const Process = (data) => {
+import * as UserApi from "../../api/UserRequest";
+import { Link } from "react-router-dom";
+export const Process = ({ data }) => {
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState();
+  useEffect(() => {
+    UserApi.getByUsername(data.employee).then((res) => {
+      setUser(res.data);
+    });
+  }, []);
+  const date = new Date(data.createdAt);
+  const time = date.toLocaleDateString("en-US", {
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return (
     <div className="Process">
-      <img src={data.data.img} alt="" />
+      <img
+        className="processPic"
+        src={data.image ? serverPublic + data.image : ""}
+        alt=""
+      />
       <div className="detail">
-        <span>
-          <b>{data.data.name}:</b>
-        </span>
-        <span> {data.data.desc}</span>
+        <img
+          className="profile"
+          src={
+            user?.profilePicture
+              ? serverPublic + user.profilePicture
+              : serverPublic + "defaultProfilePicture.jpg"
+          }
+        />
+        <Link
+          to={`/profile/${user?._id}`}
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+          }}
+        >
+          <span className="det">
+            <b>{data.employee + ": "}</b>
+            {data.desc}
+          </span>
+        </Link>
       </div>
-        <span style={{ color: "var(--gray)", fontSize: "12px",}}>{data.data.date}</span>
+      <span style={{ color: "var(--gray)", fontSize: "12px" }}>{time}</span>
     </div>
   );
 };
