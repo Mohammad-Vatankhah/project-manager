c7e2ac4a3a1845f137be04dc3923113c52c45334
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-09-01 14:19:23 +0430

    company details are dynamic now

--- client/src/Pages/CompanyPage/CompanyPage.js
+++ client/src/Pages/CompanyPage/CompanyPage.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CompanyPageLeft } from "../../Components/CompanyPageLeft/CompanyPageLeft";
 import PostSide from "../../Components/PostSide/PostSide";
 import RightSide from "../../Components/RightSide/RightSide";
 import { CompanyDetails } from "../../Components/CompanyDetails/CompanyDetails";
-
 import "./CompanyPage.css";
+import { getCompanyById } from "../../api/CompanyRequests";
+import { useParams } from "react-router-dom";
 export const CompanyPage = () => {
+  const [companyData, setCompanyData] = useState();
+  const params = useParams();
+  useEffect(() => {
+    getCompanyById(params.id).then((res) => {
+      console.log(res.data);
+      setCompanyData(res.data);
+    });
+  }, [params.id]);
   return (
     <div className="CompanyPage">
       <CompanyPageLeft />
       <div className="companyPageCenter">
-        <CompanyDetails />
+        <CompanyDetails
+          company={companyData && companyData}
+        />
         <PostSide />
       </div>
       <RightSide />
