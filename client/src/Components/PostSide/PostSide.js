320bcd9fd0ee0c3d76efd2fe1455341771fbdc3a
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-09-02 14:58:56 +0430

    company projects added

--- client/src/Components/PostSide/PostSide.js
+++ client/src/Components/PostSide/PostSide.js
@@ -3,16 +3,21 @@ import Projects from "../Projects/Projects";
 import "./PostSide.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-const PostSide = () => {
+const PostSide = ({ company }) => {
   const user = useSelector((state) => state.authReducer.authData.user);
   return (
     <div className="PostSide">
-      <Link style={{ textDecoration: "none" }} to={`/createProject/${user._id}`}>
-        <button className="button" id="create-button">
-          Create new project
-        </button>
-      </Link>
-      <Projects />
+      {!company && (
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`/createProject/${user._id}`}
+        >
+          <button className="button" id="create-button">
+            Create new project
+          </button>
+        </Link>
+      )}
+      <Projects company={company} />
     </div>
   );
 };
