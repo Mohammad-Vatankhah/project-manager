e4bb90196e7b934d61b2ea6a8e7463eeab9505bc
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-09-03 15:22:06 +0430

    modified

--- client/src/Components/ProjectDetails/ProjectDetails.js
+++ client/src/Components/ProjectDetails/ProjectDetails.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-export const ProjectDetails = ({data, img}) => {
+export const ProjectDetails = ({ data, img }) => {
   const style = img ? { marginTop: "15px" } : { marginBottom: "15px" };
   return (
     <div className="ProjectDetails" style={style}>
       <span>
-        <b>{data.employees[0]}:</b>
+        <b>{data?.company !== "" ? data?.company : data.employees[0]}:</b>
       </span>
       <span> {data.desc}</span>
     </div>
