3d904851f048991490c8dbe0b4ab36be905344ac
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-09-03 12:32:59 +0430

    employees card is dynamic now

--- client/src/Components/EmployeesCard/EmployeesCard.js
+++ client/src/Components/EmployeesCard/EmployeesCard.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import "./EmployeesCard.css"
-import { Followers } from "../../Data/FollowersData";
-
-export const EmployeesCard = () => {
+import React, { useEffect, useState } from "react";
+import "./EmployeesCard.css";
+import { AiOutlinePlus } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import { getUser } from "../../api/UserRequest";
+import { User } from "../User/User";
+export const EmployeesCard = ({ company }) => {
+  const user = useSelector((state) => state.authReducer.authData.user);
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    if (company?.employees.length > 0) {
+      company?.employees.map((e) => {
+        getUser(e).then((res) => {
+          setEmployees((prev) => [...prev, res.data]);
+        });
+      });
+    }
+  }, [company?.employees]);
   return (
-    <div className='EmployeesCard'>
-      <h3>Employees</h3>
-      {Followers.map((employee, id) => {
-        return (
-          <div className="employee">
-            <div>
-              <img src={employee.img} alt="" className="employeeImage" />
-              <div className="name">
-                <span>{employee.name}</span>
-                <span>@{employee.username}</span>
-              </div>
-            </div>
-            <button className="button" id="view-employee">View</button>
-          </div>
-        );
-      })}
+    <div className="EmployeesCard">
+      <div className="header">
+        <h3>Employees</h3>
+        {company?.owner === user._id && (
+          <AiOutlinePlus
+            style={{ cursor: "pointer", fontSize: "20px", color: "black" }}
+          />
+        )}
+      </div>
+      {company?.employees.length > 0
+        ? employees.map((employee) => (
+            <User person={employee} key={employee._id} />
+          ))
+        : "No employees available!"}
     </div>
-  )
-}
+  );
+};
